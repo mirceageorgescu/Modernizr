d74a640a41611ece1769c835bf9c950c6561858a
Ryan Seddon <seddon.ryan@gmail.com>
2012-03-09 11:44:23 +1100

    Arrrggggh, work damn you

--- test/js/server.js
+++ test/js/server.js
@@ -1,11 +1,11 @@
 var connect = require('connect'),
 	args = process.argv.slice(2),
-	folder = args[0] || '',
-	port = args[1] || '1337';	
+	folder = args[0] || '/../',
+	port = args[1] || '80';	
 
 var server = connect.createServer(
     connect.logger()
-  , connect.static(__dirname + "/"+folder)
+  , connect.static(__dirname + folder)
 ).listen(port);
 
-console.log("Server started on port %s in %s", port, "/"+folder);
+console.log("Server started on port %s in %s", port, folder);
